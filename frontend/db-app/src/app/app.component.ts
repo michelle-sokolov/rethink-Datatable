8afe70c6fea2abfaf049ba2b14e970d3e22fd991
michelle-sokolov <michellersokolov@gmail.com>
2020-11-29 21:31:32 -0800

    import dummy data + import ngx db

--- frontend/db-app/src/app/app.component.ts
+++ frontend/db-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 import * as data from './school-data.json';
 
 @Component({
--- frontend/db-app/src/app/app.module.ts
+++ frontend/db-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgxDatatableModule,
     AppRoutingModule
   ],
   providers: [],
